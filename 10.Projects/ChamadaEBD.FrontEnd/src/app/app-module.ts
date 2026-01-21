c95824cbdcb8680d18f99abfccbe1a652a19c30b
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-01-21 16:58:50 -0300

    Feat: PrimeNG e Primeicons conmfiguração inicial

--- 10.Projects/ChamadaEBD.FrontEnd/src/app/app-module.ts
+++ 10.Projects/ChamadaEBD.FrontEnd/src/app/app-module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+// PrimeNG
+import { providePrimeNG } from 'primeng/config';
+import Aura from "@primeuix/themes/aura";
+import { FilterMatchMode } from 'primeng/api';
+
+// Modules
 @NgModule({
   declarations: [
     App
@@ -14,6 +20,28 @@ import { App } from './app';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+      theme: {
+        preset: {
+          preset: Aura,
+          options: {
+            darkModeSelector: false
+          }
+        }
+      },
+      ripple: true,
+      filterMatchModeOptions: {
+        text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.ENDS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.EQUALS],
+        numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO],
+        date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_AFTER, FilterMatchMode.DATE_BEFORE]
+      },
+      translation: {
+        accept: "Sim",
+        reject: "Não",
+        cancel: "Cancelar",
+        dateFormat: "dd/mm/yyyy"
+      },
+    })
   ],
   bootstrap: [App]
 })
