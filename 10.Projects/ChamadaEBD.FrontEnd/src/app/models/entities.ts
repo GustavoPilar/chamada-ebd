ab68908a9fbe946fe3cdf0cd8b4d74bfb9cf52b1
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-01-22 16:54:00 -0300

    Feat: Entidades no FrontEnd

--- 10.Projects/ChamadaEBD.FrontEnd/src/app/models/entities.ts
+++ 10.Projects/ChamadaEBD.FrontEnd/src/app/models/entities.ts
@@ -8,3 +8,68 @@ export class User implements IEntityBase {
   phone: string;
   active: boolean;
 }
+
+export class Student implements IEntityBase {
+  id: number;
+  code: number;
+  attendanceCount: number;
+  absenceCount: number;
+  attendacePercentage: number;
+  description: string;
+  userId: number;
+  classRoomId: number;
+
+  user: User;
+  classRoom: ClassRoom
+}
+
+export class Teacher implements IEntityBase {
+  id: number;
+  code: number;
+  attendanceCount: number;
+  absenceCount: number;
+  attendacePercentage: number;
+  description: string;
+  userId: number;
+  classRoomId: number;
+
+  user: User;
+  classRoom: ClassRoom
+}
+
+export class StudentCheckin implements IEntityBase {
+  id: number;
+  code: number;
+  dateTimeCheckin: Date;
+  description: string;
+  studentId: number;
+  classRoomId: number;
+
+  student: Student;
+  classRoom: ClassRoom;
+}
+
+export class TeacherCheckin implements IEntityBase {
+  id: number;
+  code: number;
+  dateTimeCheckin: Date;
+  description: string;
+  teacherId: number;
+  classRoomId: number;
+
+  teacher: Teacher;
+  classRoom: ClassRoom;
+}
+
+export class ClassRoom implements IEntityBase {
+  id: number;
+  code: number;
+  description: string;
+  startHour: string;
+  finishHour: string;
+  StudentsCount: number;
+  teachersCount: number;
+
+  teachers: Teacher[];
+  students: Student[];
+}
