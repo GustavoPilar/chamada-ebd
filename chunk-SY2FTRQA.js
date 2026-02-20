31d12ce05167f70b6a29a4fd6beaec5f2a79c2f7
Gustavo Pilar <123991806+GustavoPilar@users.noreply.github.com>
2026-02-20 00:30:18 -0300

    Auto-generated commit

---
+++ chunk-SY2FTRQA.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-WB3SV7ZY.js";import"./chunk-3BEVSRKC.js";import"./chunk-SUHVQDRK.js";import"./chunk-NCFPMDGA.js";export{a as CrudModule};
---
+++ chunk-WDVKVVD4.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-637UBVCW.js";import"./chunk-3BEVSRKC.js";import"./chunk-NCFPMDGA.js";export{a as PagesModule};
