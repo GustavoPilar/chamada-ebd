0cfc6cf25523893074f99921e72b77c0fef7deff
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-02-20 16:35:03 -0300

    Auto-generated commit

---
+++ chunk-22AOML3Y.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-HQ5ASZXL.js";import"./chunk-3BEVSRKC.js";import"./chunk-KZXZG6I5.js";import"./chunk-NCFPMDGA.js";export{a as CrudModule};
--- chunk-MGJCD5JS.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-UUMCUPDT.js";import"./chunk-3BEVSRKC.js";import"./chunk-KZXZG6I5.js";import"./chunk-NCFPMDGA.js";export{a as CrudModule};
