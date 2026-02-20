16d6342d23c8884a27782921c680ebd01cec7651
Gustavo Pilar <123991806+GustavoPilar@users.noreply.github.com>
2026-02-20 19:25:20 -0300

    Auto-generated commit

--- chunk-6QORMD6B.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-ZTOJ55QS.js";import"./chunk-3BEVSRKC.js";import"./chunk-73GDRVW2.js";import"./chunk-4QPRMCSV.js";import"./chunk-NCFPMDGA.js";export{a as CrudModule};
---
+++ chunk-QAC2LIDZ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-MXRUM6QA.js";import"./chunk-3BEVSRKC.js";import"./chunk-73GDRVW2.js";import"./chunk-4QPRMCSV.js";import"./chunk-NCFPMDGA.js";export{a as CrudModule};
