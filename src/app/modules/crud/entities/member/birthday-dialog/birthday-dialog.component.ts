f8c76f6ef46b2e4284688599bd1a6ce648a6b365
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-02-20 13:09:14 -0300

    Feat: Aniversários de casamento

--- src/app/modules/crud/entities/member/birthday-dialog/birthday-dialog.component.ts
+++ src/app/modules/crud/entities/member/birthday-dialog/birthday-dialog.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
 import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
+import { ApiService } from "../../../../../services/api-service/api.service";
+import { MessageService } from "primeng/api";
+import { DisplayColumn } from "../../../../../models/crud/display-column";
+import { DisplayColumnType } from "../../../../../models/crud/display-column-type";
 
 @Component({
   selector: "app-birthday-dialog",
@@ -10,17 +14,27 @@ import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
 export class BirthdayDialogComponent implements OnInit {
 
   //#region Fields
-  public entities!: any[]
-  public filteredEntities!: any[];
+  public entities!: any[];
+  public weedings!: any[];
+  public filteredValues!: any[];
 
   public form!: FormGroup;
   public months!: any[];
+  public options: any[] = [
+    { id: 0, label: "Individual" },
+    { id: 1, label: "Casamento" }
+  ];
+  public alreadyLoad: boolean = false;
+  public displayColumns!: DisplayColumn[]
   //#endregion
 
   //#region Constructor
   constructor(private config: DynamicDialogConfig,
     private viewRef: DynamicDialogRef,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private messageService: MessageService,
+    private changeDetectorRef: ChangeDetectorRef
   ) {
     this.entities = this.config.data.entities.filter((x: any) => x.birthday != null).map((x: any) => x = { name: x.name, birthday: new Date(x.birthday) });
   }
@@ -30,6 +44,7 @@ export class BirthdayDialogComponent implements OnInit {
   public ngOnInit(): void {
     this.initNameMonths();
     this.initForm();
+    this.getColumns();
     this.filterEntities();
   }
   //#endregion
@@ -61,6 +76,10 @@ export class BirthdayDialogComponent implements OnInit {
       month: [
         this.months[currentIndexMonth],
         Validators.required
+      ],
+      type: [
+        this.options[0],
+        Validators.required
       ]
     });
   }
@@ -71,11 +90,132 @@ export class BirthdayDialogComponent implements OnInit {
 
   public filterEntities(): void {
     let month: number = this.form.get('month')?.value.id;
-    this.filteredEntities = this.entities.filter(x => new Date(x.birthday).getMonth() == month);
+    this.filteredValues = this.entities.filter(x => new Date(x.birthday).getMonth() == month);
+  }
+
+  public filterWeedings(): void {
+    let month: number = this.form.get('month')?.value.id;
+    this.filteredValues = this.weedings.filter(x => new Date(x.weedingDateTime).getMonth() == month);
   }
 
   public transformDate(date: Date): string {
     return date.toLocaleDateString();
+  }
+
+  public onChangeType(event: any): void {
+    this.filteredValues = [];
+    if (event.value.id == 0) {
+      this.filterEntities();
+      this.changeDetectorRef.detectChanges();
+    }
+    else {
+      if (!this.alreadyLoad) {
+        this.loadWeedings().then((result) => {
+          if (result) {
+            this.alreadyLoad = true;
+            this.getColumns();
+            this.filterWeedings();
+            this.changeDetectorRef.detectChanges();
+          }
+        }, (error) => {
+          this.messageService.add({
+            summary: "Erro ao carregar",
+            detail: "Ocorreu um erro ao carregar os casamentos.\nTente novamente.",
+            life: 3000,
+            closable: true,
+            severity: "error"
+          });
+
+          this.form.get("type")?.setValue(this.options[0]);
+        });
+      }
+      else {
+        this.filterWeedings();
+        this.changeDetectorRef.detectChanges();
+      }
+    }
+  }
+
+  public loadWeedings(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      try {
+        this.apiService.getEntities("weedingDate").then((result: any) => {
+          if (result) {
+            this.weedings = result;
+            resolve(result);
+          }
+        }, (error: any) => {
+          reject(error);
+        });
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    })
+  }
+
+  public getColumns(): void {
+    if (this.form.get("type")?.value.id == 0) {
+      this.displayColumns = [
+        {
+          label: "Nome",
+          field: "name",
+          displayColumnType: DisplayColumnType.TEXT
+        },
+        {
+          label: "Aniversário",
+          field: "birthday",
+          displayColumnType: DisplayColumnType.DATE
+        }
+      ]
+    }
+    else {
+      this.displayColumns = [
+        {
+          label: "Esposo",
+          field: "husband.name",
+          displayColumnType: DisplayColumnType.OBJECT
+        },
+        {
+          label: "Esposa",
+          field: "wife.name",
+          displayColumnType: DisplayColumnType.OBJECT
+        },
+        {
+          label: "Aniversário",
+          field: "weedingDateTime",
+          displayColumnType: DisplayColumnType.DATE
+        }
+      ]
+    }
+  }
+
+  public showObejctValue(entity: any, columnField: string): any {
+    let fields: string[] = columnField.split(".");
+    let currentValue: any = entity;
+
+    fields.forEach((field: string) => {
+      currentValue = currentValue[field];
+    });
+
+    return currentValue;
+  }
+
+  public showDefaultValue(value?: string): any {
+    return value;
+  }
+
+  public showDateValue(date?: string) {
+    return new Date(date!).toLocaleDateString();
+  }
+
+  public onChangeMonth(): void {
+    if (this.form.get('type')?.value.id == 0) {
+      this.filterEntities();
+    }
+    else {
+      this.filterWeedings();
+    }
   }
   //#endregion
 }
