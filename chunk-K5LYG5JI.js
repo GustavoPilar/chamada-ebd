a7c653ff41f8d0bc961665cd931d0031304f2f09
Gustavo Pilar <123991806+GustavoPilar@users.noreply.github.com>
2026-03-12 20:46:35 -0300

    Auto-generated commit

---
+++ chunk-K5LYG5JI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WIPJ47AZ.js";import"./chunk-EYZ7U2OZ.js";import"./chunk-U4EO7VLT.js";import"./chunk-EPYWNYA6.js";import"./chunk-NCFPMDGA.js";export{a as TransferClassDialogComponent};
